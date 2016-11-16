8756f2a0e2a4fa898609ba5158b5b6b10fb6a527
netznarkose <mmaintz@posteo.de>
2016-11-16 15:09:18 +0100

    make initialize specs pass

--- lib/symbol_count_graph.rb
+++ lib/symbol_count_graph.rb
@@ -1,10 +1,19 @@
 class SymbolCountGraph
+  attr_reader :data, :options
 
-  def initialize(data, options={})
+  def initialize(data, options={columns: 10})
+    validate_arguments(data, options)
+    @data = data
+    @options = options 
   end
 
   def render
-    'x'
+    'x' * @data[:x]
   end
 
+  def validate_arguments(data, options)
+    raise ArgumentError if data.empty?
+    raise ArgumentError unless data.is_a? Hash
+    raise ArgumentError unless options.is_a? Hash
+  end
 end
