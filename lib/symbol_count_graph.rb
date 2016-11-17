7d8667254f56c3d54d5c92f153b6238f6f0b7f3c
netznarkose <mmaintz@posteo.de>
2016-11-17 16:31:35 +0100

    add prepare_data method

--- lib/symbol_count_graph.rb
+++ lib/symbol_count_graph.rb
@@ -1,10 +1,11 @@
 class SymbolCountGraph
-  attr_reader :data, :options
+  attr_reader :data, :options, :prepared_data
 
   def initialize(data, options = { columns: 10 })
     validate_arguments(data, options)
     @data = data
     @options = options
+    prepare_data
   end
 
   def render
@@ -17,5 +18,13 @@ class SymbolCountGraph
     raise ArgumentError if data.empty?
     raise ArgumentError unless data.is_a? Hash
     raise ArgumentError unless options.is_a? Hash
+  end
+
+  def prepare_data
+    prepared_data = []
+    @data.each do |key, value|
+      value.times { |_val| prepared_data << key.to_s }
+    end
+    @prepared_data = prepared_data.each_slice(@options[:columns]).to_a
   end
 end
