0996b1cade7d0d05065eef5ff739b09e380e5baf
netznarkose <mmaintz@posteo.de>
2016-11-22 14:44:31 +0100

    delete initialize and prepare_data_specs
    from symbol_count_graph_spec

--- spec/symbol_count_graph_spec.rb
+++ spec/symbol_count_graph_spec.rb
@@ -3,69 +3,6 @@ require 'symbol_count_graph'
 
 RSpec.describe SymbolCountGraph do
 
-  describe '#initialize' do
-    it 'raises an error when no data is provided' do
-      expect{ SymbolCountGraph.new }.to raise_error(ArgumentError)
-    end
-    it 'calls the validate_arguments method' do
-      expect_any_instance_of(SymbolCountGraph).to receive(:validate_arguments)
-      SymbolCountGraph.new({})
-    end
-    it 'raises an error when the data hash is empty' do
-      expect{ SymbolCountGraph.new({}) }.to raise_error(ArgumentError)
-    end
-    it 'raises an error when the data is not a hash' do
-      expect{ SymbolCountGraph.new('x') }.to raise_error(ArgumentError)
-    end
-    it 'raises an error when the options are not a hash' do
-      expect{ SymbolCountGraph.new({ x: 2 }, 'x') }.to raise_error(ArgumentError)
-    end
-
-    it 'stores the data in an instance attribute' do
-      graph = SymbolCountGraph.new({ x: 2 })
-      expect(graph.data).to eq({ x: 2 })
-    end
-    it 'stores the options in an instance attribute' do
-      graph = SymbolCountGraph.new({ x: 2 }, { columns: 2 })
-      expect(graph.options).to eq({ columns: 2 })
-    end
-    it 'provides default options' do
-      graph = SymbolCountGraph.new({ x: 2 })
-      expect(graph.options[:columns]).to eq(10)
-    end
-    it 'accepts numbers as strings' do
-      expect{ SymbolCountGraph.new({ x: "2" }) }.to_not raise_error(NoMethodError)
-    end
-    it 'raises an error when value is not an Integer' do
-      expect{ SymbolCountGraph.new({ x: '@$' }) }.to raise_error(ArgumentError)
-    end
-    it 'raises an error when a collection of values contains a Non-Integer' do
-      expect{ SymbolCountGraph.new({ a: 12, x: '@$' }) }.to raise_error(ArgumentError)
-    end
-  end
-
-  describe '#prepare_data' do
-    it 'calls the prepare_data method' do
-      expect_any_instance_of(SymbolCountGraph).to receive(:prepare_data)
-      SymbolCountGraph.new({x: 1})
-    end
-    it 'creates the prepared_data for simple keys' do
-      graph = SymbolCountGraph.new({ x: 3, o: 2 }, { columns: 2 })
-      expect(graph.prepared_data).to eq([
-        ['x', 'x'],
-        ['x', 'o'],
-        ['o']
-      ])
-    end
-    it 'creates the prepared_data for complex keys' do
-      graph = SymbolCountGraph.new({ '#FF0000' => 2, '#00FF00' => 2 }, { columns: 2 })
-      expect(graph.prepared_data).to eq([
-        ['#FF0000', '#FF0000'],
-        ['#00FF00', '#00FF00']
-      ])
-    end
-  end
-
   describe '#render' do
     it 'outputs x when x: 1' do
       graph = SymbolCountGraph.new({ x: 1 })
