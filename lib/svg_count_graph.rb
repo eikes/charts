5899d47d86a85fdfcc695176e39e035449f30984
netznarkose <mmaintz@posteo.de>
2016-11-22 16:27:22 +0100

    implement svg rendering
    it does not work yet!

--- lib/svg_count_graph.rb
+++ lib/svg_count_graph.rb
@@ -1,6 +1,10 @@
 require_relative 'count_graph'
+require 'victor'
 class SvgCountGraph < CountGraph
 
   def render
+    svg = SVG.new width: '100%', height: '100%'
+    svg.circle cx: 10, cy: 10, r: 10, fill: '#FF0000'
+    svg.render
   end
 end
--- spec/svg_count_graph_spec.rb
+++ spec/svg_count_graph_spec.rb
@@ -1,7 +1,33 @@
 require 'spec_helper'
 require 'svg_count_graph'
+require 'capybara/rspec'
 
 RSpec.describe SvgCountGraph do
+  include Capybara::RSpecMatchers
+  
+  let(:subject) { SvgCountGraph.new(data).render }
 
-  describe '#render' do
+  context 'one circle' do
+    let(:data) { { "#FF0000" => 1 } }
+    it 'sets the SVG header' do
+      expect(subject).to match /DOCTYPE svg PUBLIC/
+      expect(subject).to match /svg width="100%" height="100%"/
+    end
+    it 'renders one circle with a radius of ten' do
+      expect(subject).to have_css('circle[r="10"]')
+    end
+    it 'renders one circle with the correct color' do
+      expect(subject).to have_css('circle[fill="#FF0000"]')
+    end
+  end
+  context 'two different circles' do
+    let(:data) { { "#FF0000" => 1, "#00FF00" => 1 } }
+    # it 'renders two circles' do
+    #   raise
+    # end
+    it 'renders two circles with the correct color' do
+      expect(subject).to have_css('circle[cx="10"][cy="10"][fill="#FF0000"]')
+      expect(subject).to have_css('circle[cx="30"][cy="10"][fill="#00FF00"]')
+    end
+  end
 end
