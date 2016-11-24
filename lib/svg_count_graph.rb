5ffdcc3d8caaa5ffdb947652f2b2e22cb8c97319
netznarkose <mmaintz@posteo.de>
2016-11-24 10:33:47 +0100

    refactor class svg_count_graph

--- lib/svg_count_graph.rb
+++ lib/svg_count_graph.rb
@@ -1,31 +1,53 @@
 require_relative 'count_graph'
 require 'victor'
+
 class SvgCountGraph < CountGraph
-  def render
-    width; height
-    svg = SVG.new width: @width, height: @height
-    radius = @options[:radius]
-    prepared_data.each_with_index do |row, row_count|
-      row.each_with_index do |column, column_count|
-        cx = radius + 2 * column_count * radius
-        cy = radius + 2 * row_count * radius
-        svg.circle cx: cx, cy: cy, r: radius, fill: column
-      end
-    end
-    svg.render
-  end
+
+  attr_reader :svg
 
   def default_options
     super.merge(radius: 10)
   end
 
+  def render
+    pre_draw
+    draw
+    post_draw
+  end
+
+  def pre_draw
+    @svg = SVG.new width: width, height: height
+  end
+
+  def draw
+    prepared_data.each_with_index do |row, row_count|
+      row.each_with_index do |color, column_count|
+        draw_item(column_count, row_count, color)
+      end
+    end
+  end
+
+  def post_draw
+    svg.render
+  end
+
+  def draw_item(column_count, row_count, color)
+    cx = radius + 2 * column_count * radius
+    cy = radius + 2 * row_count * radius
+    svg.circle cx: cx, cy: cy, r: radius, fill: color
+  end
+
   def width
-    column_count = prepared_data.first.count
-    @width = column_count * 2 * @options[:radius]
+    prepared_data.first.count * 2 * radius
   end
 
   def height
-    row_count = prepared_data.count
-    @height = row_count * 2 * @options[:radius]
+    prepared_data.count * 2 * radius
   end
+
+  def radius
+    @options[:radius]
+  end
+
 end
+
