1a3ae0154591d847e8d80c2128d7545e4bd48a35
Eike Send <eike.send@gmail.com>
2016-11-25 20:06:55 +0100

    Cleans up ImageGraph

--- lib/image_graph.rb
+++ lib/image_graph.rb
@@ -4,12 +4,8 @@ require 'victor'
 module ImageGraph
   attr_reader :graph
 
-  def is_svg?
-    options[:type] == :svg
-  end
-
   def pre_draw
-    @graph = is_svg? ? SvgGraph.new(width, height) : BitmapGraph.new(width, height)
+    @graph = options[:type] == :svg ? SvgGraph.new(width, height) : BitmapGraph.new(width, height)
   end
 
   def post_draw
@@ -23,51 +19,61 @@ module ImageGraph
 
   class SvgGraph
     attr_reader :svg
+
     def initialize(width, height)
       @svg = SVG.new width: width, height: height
     end
+
     def render
       svg.render
     end
+
     def save(filename)
       svg.save filename
     end
+
     def line(x1, y1, x2, y2, style)
-      svg.line style.merge({ x1: x1, y1: y1, x2: x2, y2: y2 })
+      svg.line style.merge(x1: x1, y1: y1, x2: x2, y2: y2)
     end
+
     def circle(cx, cy, radius, style)
-      svg.circle style.merge({ cx: cx, cy: cy, r: radius })
+      svg.circle style.merge(cx: cx, cy: cy, r: radius)
     end
   end
 
   class BitmapGraph
     attr_reader :canvas, :image
+
     def initialize(width, height)
       @image = Magick::ImageList.new
       image.new_image(width, height)
       @canvas = Magick::Draw.new
     end
+
     def render
       canvas.draw(image)
       image.to_blob { |attrs| attrs.format = 'PNG' }
     end
+
     def save(filename)
       canvas.draw(image)
       image.write filename
     end
+
     def line(x1, y1, x2, y2, style)
-      set_canvas_style style
+      apply_canvas_style style
       canvas.line x1, y1, x2, y2
     end
+
     def circle(cx, cy, radius, style)
-      set_canvas_style style
+      apply_canvas_style style
       canvas.circle cx, cy, cx - radius, cy
     end
-    def set_canvas_style(style)
+
+    def apply_canvas_style(style)
       style.each do |key, value|
         canvas.send key, value
       end
     end
   end
-
 end
