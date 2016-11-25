811f357d41e8e3bfacfda8fbbc688d4a7b9ca496
Eike Send <eike.send@gmail.com>
2016-11-25 13:35:20 +0100

    refactors CrossCountGraph, adds flag to bin/graph_tool

--- lib/cross_count_graph.rb
+++ lib/cross_count_graph.rb
@@ -1,12 +1,23 @@
-require_relative 'image_count_graph'
+require_relative 'count_graph'
+require_relative 'image_graph'
 
-class CrossCountGraph < ImageCountGraph
+class CrossCountGraph < CountGraph
+  include ImageGraph
 
   def draw_item(x, y, color)
-    width = @options[:item_width] - 8
-    height = @options[:item_height] - 8
-    cross(x + 4, y + 4, x + width, y + height, color)
+    left = x + 4
+    right = x + @options[:item_width] - 4
+    top = y + 4
+    bottom = y + @options[:item_height] - 4
+    if is_svg?
+      svg.line x1: left, y1: top, x2: right, y2: bottom, stroke: color, stroke_width: 6, stroke_linecap: 'round'
+      svg.line x1: left, y1: bottom, x2: right, y2: top, stroke: color, stroke_width: 6, stroke_linecap: 'round'
+    else
+      canvas.stroke color
+      canvas.stroke_width 6
+      canvas.stroke_linecap 'round'
+      canvas.line left, top, right, bottom
+      canvas.line left, bottom, right, top
+    end
   end
-
 end
-
