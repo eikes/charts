2657337a241a64432c570a636045b9bcf96946e7
Eike Send <eike.send@gmail.com>
2016-11-28 23:51:05 +0100

    Fixes easy rubocop complaints

--- lib/bar_graph.rb
+++ lib/bar_graph.rb
@@ -7,14 +7,12 @@ class BarGraph < Graph
   attr_reader :max, :min, :set_count, :bars_pers_set, :bar_count, :base_line
 
   def default_options
-    super.merge({
-      width:  600,
-      height: 400,
-      include_zero: true,
-      outer_margin: 50,
-      group_margin: 20,
-      bar_margin: 1
-    })
+    super.merge(width:  600,
+                height: 400,
+                include_zero: true,
+                outer_margin: 50,
+                group_margin: 20,
+                bar_margin: 1)
   end
 
   def validate_arguments(data, options)
@@ -28,28 +26,24 @@ class BarGraph < Graph
     @bar_count = set_count * bars_pers_set
 
     @max = options[:max]
-    if !@max
+    unless @max
       @max = data.map(&:max).max
-      if @max < 0 && options[:include_zero]
-        @max = 0
-      end
+      @max = 0 if @max.negative? && options[:include_zero]
     end
 
     @min = options[:min]
-    if !@min
+    unless @min
       @min = data.map(&:min).min
-      if @min > 0 && options[:include_zero]
-        @min = 0
-      end
+      @min = 0 if @min.positive? && options[:include_zero]
     end
 
     @base_line = (-min.to_f) / (max - min) # zero value mapped
-    @base_line = 0 if @base_line < 0
+    @base_line = 0 if @base_line.negative?
     @base_line = 1 if @base_line > 1
 
-    prepared_data = data.map do |set|
+    data.map do |set|
       set.map do |item|
-        #normalize data to a value between 0 and 1
+        # normalize data to a value between 0 and 1
         (item.to_f - min) / (max - min)
       end
     end
@@ -59,7 +53,6 @@ class BarGraph < Graph
     draw_background
     prepared_data.each_with_index do |set, set_nr|
       set.each_with_index do |data_point, data_point_nr|
-        bar_number = set_nr + data_point_nr * set_count
         draw_bar set_nr, data_point_nr, data_point
       end
     end
@@ -75,14 +68,14 @@ class BarGraph < Graph
     h = inner_height * (data_point - base_line).abs
 
     x = inner_left + bar_margin + (all_bars_width * bar_number / bar_count).floor.to_i + group_margin * data_point_nr
-    y = inner_top + inner_height * ([(1 - data_point), (1 - base_line)].min)
+    y = inner_top + inner_height * [(1 - data_point), (1 - base_line)].min
 
-    renderer.rect x, y, w, h, { fill: colors[set_nr], class: 'bar' }
+    renderer.rect x, y, w, h, fill: colors[set_nr], class: 'bar'
   end
 
   def draw_background
-    renderer.rect 0, 0, width, height, { fill: '#EEEEEE' }
-    renderer.rect inner_left, inner_top, inner_width, inner_height, { fill: '#FFFFFF' }
+    renderer.rect 0, 0, width, height, fill: '#EEEEEE'
+    renderer.rect inner_left, inner_top, inner_width, inner_height, fill: '#FFFFFF'
   end
 
   def inner_left
@@ -118,6 +111,7 @@ class BarGraph < Graph
   end
 
   def colors
-    options[:colors] || ['#e41a1d','#377eb9','#4daf4b','#984ea4','#ff7f01','#ffff34','#a65629','#f781c0','#888888']
+    default_colors = ['#e41a1d', '#377eb9', '#4daf4b', '#984ea4', '#ff7f01', '#ffff34', '#a65629', '#f781c0', '#888888']
+    options[:colors] || default_colors
   end
 end
