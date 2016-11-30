95b891533b2d29cd2501f1dc5e18c9ae8318809f
netznarkose <mmaintz@posteo.de>
2016-11-30 11:37:33 +0100

    implement inner margin

--- lib/count_graph.rb
+++ lib/count_graph.rb
@@ -2,7 +2,7 @@ require_relative 'graph'
 
 class CountGraph < Graph
   def default_options
-    super.merge(columns: 10, item_width: 20, item_height: 20, inner_margin: 2)
+    super.merge(columns: 10, item_width: 20, item_height: 20, inner_margin: 6)
   end
 
   def validate_arguments(data, options)
@@ -22,11 +22,15 @@ class CountGraph < Graph
   def draw
     prepared_data.each_with_index do |row, row_count|
       row.each_with_index do |color, column_count|
-        x = column_count * item_width
-        y = row_count * item_height
+        x = column_count * (item_width + inner_margin)
+        y = row_count * (item_height + inner_margin)
         draw_item(x, y, color)
       end
     end
+  end
+
+  def inner_margin
+    @options[:inner_margin]
   end
 
   def draw_item(_x, _y, _color)
@@ -34,11 +38,11 @@ class CountGraph < Graph
   end
 
   def width
-    prepared_data.first.count * item_width
+    prepared_data.first.count * (item_width + inner_margin)
   end
 
   def height
-    prepared_data.count * item_height
+    prepared_data.count * (item_height + inner_margin)
   end
 
   def item_width
