73090a06c13a78f7254a1ace0e2830e2b6868084
Eike Send <eike.send@gmail.com>
2016-12-04 22:22:51 +0100

    Uses 'proc' instead of 'Proc.new' in Graph class

--- lib/graph.rb
+++ lib/graph.rb
@@ -49,7 +49,7 @@ class Graph
 
   def create_options_methods
     options.each do |key, value|
-      self.class.send(:define_method, key, Proc.new { value })
+      self.class.send(:define_method, key, proc { value })
     end
   end
 end
