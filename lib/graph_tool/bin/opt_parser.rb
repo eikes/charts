1421e165cb61bdec0e10812bdad893aad60a78e8
Eike Send <eike.send@gmail.com>
2016-12-05 23:03:14 +0100

    Improves bin/ class' code quality

--- lib/graph_tool/bin/opt_parser.rb
+++ lib/graph_tool/bin/opt_parser.rb
@@ -1,11 +1,10 @@
 require 'optparse'
 
 class GraphTool::OptParser
-
   attr_reader :args, :options, :parser
 
-  FORMATS = [:txt, :svg, :png, :jpg, :gif]
-  STYLES = [:circle, :cross, :manikin]
+  FORMATS = [:txt, :svg, :png, :jpg, :gif].freeze
+  STYLES = [:circle, :cross, :manikin].freeze
 
   def initialize(args)
     @args = args.empty? ? ['--help'] : args
@@ -28,87 +27,91 @@ class GraphTool::OptParser
   end
 
   def post_process_options
-    unless options[:help]
-      if options[:filename]
-        type = options[:filename].match(/.*\.(#{FORMATS.join("|")})/)
-        if type
-          options[:type] = type[1].to_sym
-        else
-          options[:type] = false
-        end
-      end
+    return if options[:help]
+    return unless options[:filename]
+    type = options[:filename].match(/.*\.(#{FORMATS.join("|")})/)
+    if type
+      options[:type] = type[1].to_sym
+    else
+      options[:type] = false
     end
   end
 
   def validate_options
-    unless options[:help]
-      unless options[:type]
-        fail 'No type provided. Set a type with --type flag or by setting a valid --filename'
-      end
-      unless options[:data]
-        fail "No data provided. Please pass in data using the --data flag: 'bin/graph_tool -d Red:8,Gold:7'"
-      end
+    return if options[:help]
+    unless options[:type]
+      raise 'No type provided. Set a type with --type flag or by setting a valid --filename'
+    end
+    unless options[:data]
+      raise "No data provided. Please pass in data using the --data flag: 'bin/graph_tool -d Red:8,Gold:7'"
     end
   end
 
   def opt_parser
     OptionParser.new do |opts|
       opts.banner = 'Usage: bin/graph_tool [options]'
-      opts.on('-d DATA',
-              '--data DATA',
-              Array,
-              "Provide multiple data points, ie: 'bin/graph_tool -d Red:8,Gold:7'"
-             ) do |data|
+      opts.on(
+        '-d DATA',
+        '--data DATA',
+        Array,
+        "Provide multiple data points, ie: 'bin/graph_tool -d Red:8,Gold:7'"
+      ) do |data|
         options[:data] = data.map { |d| d.split(':') }.to_h
       end
-      opts.on('-f FILENAME',
-              '--filename FILENAME',
-              "Set the filename the result is stored in. Supported formats are: : #{FORMATS.join(', ')}"
-             ) do |filename|
+      opts.on(
+        '-f FILENAME',
+        '--filename FILENAME',
+        "Set the filename the result is stored in. Supported formats are: : #{FORMATS.join(', ')}"
+      ) do |filename|
         options[:filename] = filename
       end
-      opts.on('-s STYLE',
-              '--style STYLE',
-              STYLES,
-              "Choose the graph style: #{STYLES.join(', ')}"
-             ) do |style|
+      opts.on(
+        '-s STYLE',
+        '--style STYLE',
+        STYLES,
+        "Choose the graph style: #{STYLES.join(', ')}"
+      ) do |style|
         options[:style] = style
       end
-      opts.on('-c COLUMNS',
-              '--columns COLUMNS',
-              Integer,
-              'Set number of columns'
-             ) do |columns|
+      opts.on(
+        '-c COLUMNS',
+        '--columns COLUMNS',
+        Integer,
+        'Set number of columns'
+      ) do |columns|
         options[:columns] = columns
       end
-      opts.on('-w WIDTH',
-              '--item-width WIDTH',
-              Integer,
-              'Sets the width of the individual item'
-             ) do |item_width|
+      opts.on(
+        '-w WIDTH',
+        '--item-width WIDTH',
+        Integer,
+        'Sets the width of the individual item'
+      ) do |item_width|
         options[:item_width] = item_width
       end
-      opts.on('-h HEIGHT',
-              '--item-height HEIGHT',
-              Integer,
-              'Sets the height of the individual item'
-             ) do |item_height|
+      opts.on(
+        '-h HEIGHT',
+        '--item-height HEIGHT',
+        Integer,
+        'Sets the height of the individual item'
+      ) do |item_height|
         options[:item_height] = item_height
       end
-      opts.on('-t TYPE',
-              '--type TYPE',
-              FORMATS,
-              "If no filename is provided, output is sent to STDOUT, choose the format: #{FORMATS.join(', ')}"
-             ) do |type|
+      opts.on(
+        '-t TYPE',
+        '--type TYPE',
+        FORMATS,
+        "If no filename is provided, output is sent to STDOUT, choose the format: #{FORMATS.join(', ')}"
+      ) do |type|
         options[:type] = type
       end
-      opts.on('--help',
-              'Prints this help'
-             ) do
+      opts.on(
+        '--help',
+        'Prints this help'
+      ) do
         puts opts
         options[:help] = true
       end
     end
   end
-
 end
