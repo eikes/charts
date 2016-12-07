5c712e9828786c5473095fc08249f2b74b27a121
Eike Send <eike.send@gmail.com>
2016-12-07 11:55:27 +0100

    Change test for bin/graph_tool to actually run the script

--- spec/graph_tool/bin/graph_tool_spec.rb
+++ spec/graph_tool/bin/graph_tool_spec.rb
@@ -1,25 +1,28 @@
 require 'spec_helper'
 
 RSpec.describe 'bin/graph_tool' do
-  let(:graph) { dispatcher.graph }
-  let(:dispatcher) { GraphTool::Dispatcher.new(options) }
-  let(:options) { parser.parse }
-  let(:parser) { GraphTool::OptParser.new(args) }
   let(:data_args) { GraphTool::OptParser::DATA_EXAMPLE_ARGS.split(' ') }
   let(:color_args) { GraphTool::OptParser::COLOR_EXAMPLE_ARGS.split(' ') }
 
-  context 'data args are provided' do
-    let(:args) { data_args }
-    it 'creates an svg output' do
-      expect(graph.render).to match(/DOCTYPE svg PUBLIC/)
+  context 'the important part of the script is loaded' do
+    let(:script) do
+      file_content = File.read('bin/graph_tool')
+      code = file_content.split("# code #")
+      code[1]
     end
-  end
-
-  context 'data and colors args are provided' do
-    let(:args) { data_args + color_args }
-    it 'creates an svg output' do
-      expect(graph.render).to match(/red/)
-      expect(graph.render).to match(/gold/)
+    context 'no args are provided' do
+      let(:args) { [] }
+      it 'prints the help message to stdout' do
+        expect { eval script }.to output(/Usage: bin\/graph_tool \[options\]/).to_stdout
+      end
+    end
+    context 'data and colors args are provided' do
+      let(:args) { data_args + color_args }
+      it 'prints the svg to stdout' do
+        expect { eval script }.to output(/DOCTYPE svg PUBLIC/).to_stdout
+        expect { eval script }.to output(/red/).to_stdout
+        expect { eval script }.to output(/gold/).to_stdout
+      end
     end
   end
 
